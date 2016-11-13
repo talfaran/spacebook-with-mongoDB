b2f257dea1cfa68f5b8bffb4d3e60926d9927420
Steven Winston <swinston100@hotmail.com>
2016-11-13 15:03:55 +0200

    small fixes and improved toggle comment function

--- main.js
+++ main.js
@@ -2,24 +2,24 @@ var SpacebookApp = function() {
 
     var posts = [];
     var currentId = 0;
-    var $posts = $(".posts")
+    var $posts = $(".posts");
 
     var post = function(btn) {
 
         var input = $(btn).closest("form").find("input");
-        var post = input.val()
+        var post = input.val();
         if (post === "") {
             alert("Please enter text!");
 
         } else {
-            _addPost(post)
-            input.val("")
-            _updatePosts()
+            _addPost(post);
+            input.val("");
+            _renderPosts();
         }
-    }
+    };
 
 
-    function _updatePosts() {
+    function _renderPosts() {
 
         $posts.empty();
         var source = $('#post-template').html();
@@ -29,16 +29,17 @@ var SpacebookApp = function() {
             var newHTML = template(post);
             // append our new html to the page
             $posts.append(newHTML);
-        })
+        });
     }
 
     function _addPost(newPost) {
-        posts.push({ text: newPost, id: currentId, comments: [] })
-        currentId++
+        posts.push({ text: newPost, id: currentId, comments: [] });
+        currentId++;
     }
 
-    function _updateCommentCount($commentsList, postIndex) {
-        $post = $commentsList.closest('.post')
+    function _renderSinglePost($commentsList, postIndex) {
+        //this function re-renders the post so that the comment count can updates
+        $post = $commentsList.closest('.post');
         var source = $('#post-template').html();
         var template = Handlebars.compile(source);
         var newHTML = template(posts[postIndex]);
@@ -47,23 +48,24 @@ var SpacebookApp = function() {
         //append the replacement post leaving the old div.post wrapper in place
         $post.append(newHTML);
         //to delete the old wrapper, step down a level and remove the parent 
-        $replacementPost = $post.children()
+        $replacementPost = $post.children();
         $replacementPost.unwrap();
         //we deleted the old $commentsList so we need to return the new list
-        return $replacementPost.find('.comments-list')
+        return $replacementPost.find('.comments-list');
     }
 
-    function _updateComments($commentsList, postIndex) {
-        $commentsList = _updateCommentCount($commentsList, postIndex) //need this to update comment count, yuck!
-        $commentsList.empty()
+    function _renderComments($commentsList, postIndex) {
+        //first to update the commentCount we need to rerender the whole post, yuck!
+        $commentsList = _renderSinglePost($commentsList, postIndex);
+        $commentsList.empty();
         var source = $('#comment-template').html();
         var template = Handlebars.compile(source);
         posts[postIndex].comments.forEach(function(comment) {
             var newHTML = template(comment);
             // append our new html to the page
             $commentsList.append(newHTML);
-        })
-        return $commentsList
+        });
+        return $commentsList;
     }
 
     var removePost = function(btn) {
@@ -72,10 +74,10 @@ var SpacebookApp = function() {
         var id = $(btn).closest(".post").data().id;
         //remove post from array
         posts = posts.filter(function(post) {
-            return post.id != id
-        })
-        _updatePosts();
-    }
+            return post.id != id;
+        });
+        _renderPosts();
+    };
 
     var addComment = function(btn) {
 
@@ -83,7 +85,7 @@ var SpacebookApp = function() {
         var $user = $(btn).siblings('.name');
 
         if ($comment.val() === "" || $user.val() === "") {
-            alert("Please enter your name and a comment!")
+            alert("Please enter your name and a comment!");
             return;
         }
 
@@ -97,17 +99,17 @@ var SpacebookApp = function() {
         $comment.val("");
         $user.val("");
 
-        $commentsList = _updateComments($commentsList, postIndex)
+        $commentsList = _renderComments($commentsList, postIndex);
         $commentsList.addClass('show');
 
-        //viewComments($(btn).siblings('.view-comments'))
-    }
+        //viewComments($(btn).siblings('.view-comments'));
+    };
 
 
     // var viewComments = function(btn) {
     //     var $commentsList = $(btn).closest('.post').find('.comments-list')
     //     var postIndex = $(btn).closest('.post').index();
-    //     $commentsList = _updateComments($commentsList, postIndex)
+    //     $commentsList = _renderComments($commentsList, postIndex)
     //     $hideBtn = $commentsList.closest(".post").find('.hide-comments')
     //     $hideBtn.removeClass('hidden')
     //     $hideBtn.siblings('.view-comments').addClass('hidden');
@@ -122,24 +124,25 @@ var SpacebookApp = function() {
 
     var toggleComments = function(btn) {
         var $commentsList = $(btn).closest('.post').find('.comments-list');
-        //if the comments list is hidden we first need to build it!
-        //TODO - stop this rebuilding if already built!
-        if (!$commentsList.hasClass('show')) {
+        //render comments if list is hidden and there are no comments in the list!
+        if (!$commentsList.hasClass('show') && $commentsList.children().length === 0) {
             var postIndex = $(btn).closest('.post').index();
-            $commentsList = _updateComments($commentsList, postIndex)
+            $commentsList = _renderComments($commentsList, postIndex);
         }
         $commentsList.toggleClass('show');
-    }
+    };
 
     var deleteComment = function(btn) {
-        var $commentsList = $(btn).closest('.post').find('.comments-list')
+        var $commentsList = $(btn).closest('.post').find('.comments-list');
         var postIndex = $(btn).closest('.post').index();
         var commentIndex = $(btn).closest('.comment').index();
 
         //remove the comment and refresh the view
         posts[postIndex].comments.splice(commentIndex, 1);
-        _updateComments($commentsList, postIndex)
-    }
+        $commentsList = _renderComments($commentsList, postIndex);
+        //finally show the list minus the comment that was removed
+        $commentsList.addClass('show');
+    };
 
     return {
         post: post,
@@ -149,13 +152,13 @@ var SpacebookApp = function() {
         // hideComments: hideComments,
         deleteComment: deleteComment,
         toggleComments: toggleComments
-    }
-}
+    };
+};
 
 
 var app = SpacebookApp();
 var post = app.post;
-var $posts = $(".posts")
+var $posts = $(".posts");
 
 $posts.on('click', '.remove-post', function() {
     app.removePost(this);
